308d1dda9b43d67c79914cab9ae0ddca177599b3
vrcats <vrcats@fcf8acc2-b06a-11dd-a5e3-f1c34dd4e8bc>
2008-11-28 06:16:50 +0000

    lots of

--- moc_stickerwindow.cpp
+++ moc_stickerwindow.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'stickerwindow.h'
 **
-** Created: Thu Oct 30 15:12:14 2008
-**      by: The Qt Meta Object Compiler version 60 (Qt 4.5.0)
+** Created: Wed Nov 26 16:16:45 2008
+**      by: The Qt Meta Object Compiler version 59 (Qt 4.4.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,8 +10,8 @@
 #include "stickerwindow.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'stickerwindow.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 60
-#error "This file was generated using the moc from 4.5.0. It"
+#elif Q_MOC_OUTPUT_REVISION != 59
+#error "This file was generated using the moc from 4.4.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -23,15 +23,19 @@ static const uint qt_meta_data_stickerWindow[] = {
        1,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   10, // methods
        0,    0, // properties
        0,    0, // enums/sets
+
+ // slots: signature, parameters, type, tag, flags
+      15,   14,   14,   14, 0x08,
+      29,   14,   14,   14, 0x08,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_stickerWindow[] = {
-    "stickerWindow\0"
+    "stickerWindow\0\0saveContent()\0updateText()\0"
 };
 
 const QMetaObject stickerWindow::staticMetaObject = {
@@ -57,6 +61,13 @@ int stickerWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QTextEdit::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: saveContent(); break;
+        case 1: updateText(); break;
+        }
+        _id -= 2;
+    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
